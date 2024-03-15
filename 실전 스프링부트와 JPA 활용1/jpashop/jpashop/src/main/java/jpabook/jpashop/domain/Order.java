5fb01f5af706bee46cea301ab13cfa88d4b010d1
lgh000322 <kang000322@gmail.com>
2024-03-15 15:50:27 +0900

    도메인 분석 설계

---
+++ 실전 스프링부트와 JPA 활용1/jpashop/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,51 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate; // 주문시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; //주문상태 ORDER, CANCEL
+
+    //연관관계 편의 메소드(양방향일 때 사용)
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+}
