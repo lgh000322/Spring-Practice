b20a138dce8738a5f80b67f79a1f29595c135322
lgh000322 <kang000322@gmail.com>
2024-04-05 15:28:17 +0900

    서블릿으로 회원관리 웹앱만들기

---
+++ 스프링 MVC 1편/servlet/servlet/src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,63 @@
+package hello.servlet.domain.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach(){
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Member member = new Member("hello", 20);
+
+        //when
+        Member savedMember = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.findById(member.getId());
+        assertEquals(findMember,savedMember);
+    }
+
+    @Test
+    void findAll() {
+        //given
+        Member member1 = new Member("kim", 10);
+        Member member2 = new Member("lee", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        List<Member> findMembers = memberRepository.findAll();
+
+        //then
+        assertEquals(findMembers.size(),2);
+        org.assertj.core.api.Assertions.assertThat(findMembers).contains(member1, member2);
+
+    }
+
+
+    @Test
+    void findById() {
+        //given
+        Member member = new Member("kim", 10);
+        memberRepository.save(member);
+
+        //when
+        Member findMember = memberRepository.findById(member.getId());
+
+        //then
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+}
