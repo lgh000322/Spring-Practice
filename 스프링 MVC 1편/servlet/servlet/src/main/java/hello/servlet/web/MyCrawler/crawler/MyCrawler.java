ad18ece387cf9eea420ecd117b7031570effaaec
lgh000322 <kang000322@gmail.com>
2024-04-09 14:52:40 +0900

    스프링과 상관없는 셀레니움 크롤링 연습2

--- 스프링 MVC 1편/servlet/servlet/src/main/java/hello/servlet/web/MyCrawler/crawler/MyCrawler.java
+++ 스프링 MVC 1편/servlet/servlet/src/main/java/hello/servlet/web/MyCrawler/crawler/MyCrawler.java
@@ -1,12 +1,15 @@
 package hello.servlet.web.MyCrawler.crawler;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.Duration;
+import java.util.List;
 
 @Controller
 public class MyCrawler {
@@ -27,6 +30,39 @@ public class MyCrawler {
 
         // Google 홈페이지 열기
         driver.get("https://www.google.com");
+
+        WebElement htmlElement = driver.findElement(By.tagName("html"));
+        List<WebElement> allElements = htmlElement.findElements(By.xpath("//*"));
+
+        // 모든 요소를 순회하면서 출력합니다.
+        for (WebElement element : allElements) {
+            System.out.println(element.getTagName() + ": " + element.getText());
+        }
+
+        // 태그명으로 요소 찾기
+        try {
+            WebElement h1Element = driver.findElement(By.tagName("h1"));
+            System.out.println("h1 태그의 텍스트: " + h1Element.getText());
+        } catch (Exception e) {
+            System.out.println("MyCrawler.getCrawlContent_E1");
+        }
+
+        // 클래스명으로 요소 찾기
+        try {
+            WebElement exampleElement = driver.findElement(By.className("example"));
+            System.out.println(".example 클래스를 가진 요소의 텍스트: " + exampleElement.getText());
+        } catch (Exception e) {
+            System.out.println("MyCrawler.getCrawlContent_E2");
+        }
+
+        // 네임명으로 요소 찾기
+        try {
+            WebElement inputElement = driver.findElement(By.name("username"));
+            System.out.println("name 속성이 'username'인 요소의 태그명: " + inputElement.getTagName());
+        } catch (Exception e) {
+            System.out.println("MyCrawler.getCrawlContent_E3");
+        }
+
 
         // 페이지 제목 출력
         System.out.println("Page title: " + driver.getTitle());
