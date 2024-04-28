07ed51f2aabcdd25d75d51fcb02a6a74d33f70cf
lgh000322 <kang000322@gmail.com>
2024-04-28 15:24:08 +0900

    스프링부트-오류페이지 처리

--- 스프링 MVC 2편/exception/exception/src/main/java/hello/exception/servlet/ServletExceptionController.java
+++ 스프링 MVC 2편/exception/exception/src/main/java/hello/exception/servlet/ServletExceptionController.java
@@ -18,6 +18,10 @@ public class ServletExceptionController {
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404, "404오류");
     }
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400, "400오류");
+    }
 
     @GetMapping("/error-500")
     public void error500(HttpServletResponse response) throws IOException {
