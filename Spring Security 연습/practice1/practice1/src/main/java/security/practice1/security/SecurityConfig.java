4b741318a41038fe35329ef1e7ee92a8f2438fd3
lgh000322 <kang000322@gmail.com>
2024-05-07 00:15:23 +0900

    스프링 시큐리티 로그인 성공, 실패등 config 설정

--- Spring Security 연습/practice1/practice1/src/main/java/security/practice1/security/SecurityConfig.java
+++ Spring Security 연습/practice1/practice1/src/main/java/security/practice1/security/SecurityConfig.java
@@ -1,25 +1,55 @@
 package security.practice1.security;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
+    //successHandler, failureHandler가 우선권을 가진다.
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+                .formLogin(form -> form
+                      /*  .loginPage("/loginPage")*/
+                        .loginProcessingUrl("/loginProc")
+                        .defaultSuccessUrl("/", false)
+                        .failureUrl("/failed")
+                        .usernameParameter("userId")
+                        .passwordParameter("passwd")
+                        .successHandler((request, response, authentication) -> {
+                            System.out.println("authentication " + authentication);
+                            response.sendRedirect("/home");
+                        })
+                        .failureHandler(new AuthenticationFailureHandler() {
+                            @Override
+                            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                                System.out.println("exception = " + exception.getMessage());
+                                response.sendRedirect("/login");
+                            }
+                        })
+                        .permitAll()
+                );
 
         return http.build();
     }
