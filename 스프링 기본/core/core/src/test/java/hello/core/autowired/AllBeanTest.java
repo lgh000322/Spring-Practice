76aab74c94a1614c41eb81f22fa507544f195fd5
lgh000322 <kang000322@gmail.com>
2024-08-01 15:19:37 +0900

    Feat: 빈 생명주기 콜백

--- 스프링 기본/core/core/src/test/java/hello/core/autowired/AllBeanTest.java
+++ 스프링 기본/core/core/src/test/java/hello/core/autowired/AllBeanTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
