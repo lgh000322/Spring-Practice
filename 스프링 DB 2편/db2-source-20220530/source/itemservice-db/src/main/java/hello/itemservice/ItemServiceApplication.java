7f040b2786debd69fbdd03c407517b81763400b1
lgh000322 <kang000322@gmail.com>
2024-08-09 17:25:27 +0900

    Feat: 데이터 접근 기술-테스트

--- 스프링 DB 2편/db2-source-20220530/source/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ 스프링 DB 2편/db2-source-20220530/source/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,12 +2,15 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -15,6 +18,7 @@ import java.util.Comparator;
 //@Import(JDBCTemplateV1Config.class)
 //@Import(JDBCTemplateV2Config.class)
 @Import(JDBCTemplateV3Config.class)
+@Slf4j
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
@@ -28,4 +32,15 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
+/*	@Bean
+	@Profile("test")
+	public DataSource dataSource() {
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");//메모리 모드
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
+	}*/
 }
