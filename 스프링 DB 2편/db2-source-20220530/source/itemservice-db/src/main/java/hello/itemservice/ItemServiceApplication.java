b026bf2701f43c67bc95b73007cd699cb4f6d90e
lgh000322 <kang000322@gmail.com>
2024-08-09 09:59:12 +0900

    Feat: JDBC 템플릿 적용

--- 스프링 DB 2편/db2-source-20220530/source/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ 스프링 DB 2편/db2-source-20220530/source/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -8,7 +8,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-@Import(MemoryConfig.class)
+import java.util.Arrays;
+import java.util.Comparator;
+
+//@Import(MemoryConfig.class)
+@Import(JDBCTemplateV1Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
