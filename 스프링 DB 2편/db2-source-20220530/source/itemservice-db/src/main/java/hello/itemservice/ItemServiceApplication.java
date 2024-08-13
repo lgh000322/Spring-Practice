8b965204fafc7dfd2d04de660440eff7c9029bdd
lgh000322 <kang000322@gmail.com>
2024-08-13 11:11:51 +0900

    Feat: Jpa 적용

--- 스프링 DB 2편/db2-source-20220530/source/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ 스프링 DB 2편/db2-source-20220530/source/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -18,7 +18,8 @@ import java.util.Comparator;
 //@Import(JDBCTemplateV1Config.class)
 //@Import(JDBCTemplateV2Config.class)
 //@Import(JDBCTemplateV3Config.class)
-@Import(MybatisConfig.class)
+//@Import(MybatisConfig.class)
+@Import(JpaConfig.class)
 @Slf4j
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
--- 스프링 DB 2편/db2-source-20220530/source/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ 스프링 DB 2편/db2-source-20220530/source/itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -1,12 +1,22 @@
 package hello.itemservice.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
+@Entity
+@Table(name = "Item")
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name",length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
