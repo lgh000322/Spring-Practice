c2a5bac38d0799a4a9076c71510ad2e199724716
lgh000322 <kang000322@gmail.com>
2024-08-20 15:12:55 +0900

    Feat: 로그 추적기 V2 - 동기화 by traceIdHolder (동시성 문제 발생)

--- 스프링 핵심원리 -고급편/advanced/advanced/src/main/java/hello/advanced/app/v3/OrderControllerV3.java
+++ 스프링 핵심원리 -고급편/advanced/advanced/src/main/java/hello/advanced/app/v3/OrderControllerV3.java
@@ -1,7 +1,9 @@
-package hello.advanced.app.v2;
+package hello.advanced.app.v3;
 
+import hello.advanced.LogTraceConfig;
 import hello.advanced.trace.TraceStatus;
 import hello.advanced.trace.hellotrace.HelloTraceV2;
+import hello.advanced.trace.logtrace.LogTrace;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,17 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class OrderControllerV2 {
+public class OrderControllerV3 {
 
-    private final OrderServiceV2 orderService;
-    private final HelloTraceV2 trace;
+    private final OrderServiceV3 orderService;
+    private final LogTrace trace;
 
-    @GetMapping("/v2/request")
+    @GetMapping("/v3/request")
     public String request(@RequestParam(name = "itemId") String itemId) {
         TraceStatus status = null;
         try {
             status = trace.begin("OrderController.request()");
-            orderService.orderItem(status.getTraceId(),itemId);
+            orderService.orderItem(itemId);
             trace.end(status);
             return "ok";
         } catch (Exception e) {
---
+++ 스프링 핵심원리 -고급편/advanced/advanced/src/test/java/hello/advanced/trace/logtrace/FieldLogTraceTest.java
@@ -0,0 +1,29 @@
+package hello.advanced.trace.logtrace;
+
+import hello.advanced.trace.TraceStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FieldLogTraceTest {
+    FieldLogTrace trace;
+
+    @BeforeEach
+    void init() {
+        trace = new FieldLogTrace();
+    }
+
+    @Test
+    public void begin_end_level2() throws Exception{
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+        trace.end(status2);
+        trace.end(status1);
+    }
+    @Test
+    public void begin_exception_level2() throws Exception{
+        TraceStatus status1 = trace.begin("hello1");
+        TraceStatus status2 = trace.begin("hello2");
+        trace.exception(status2,new IllegalStateException("예외 발생!"));
+        trace.exception(status1,new IllegalStateException("예외 발생!"));
+    }
+}
