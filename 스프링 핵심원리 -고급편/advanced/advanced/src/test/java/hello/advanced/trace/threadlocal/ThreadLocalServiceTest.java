279bbd81ff5b0e479df2d5a4d3c3c56d6aa8421b
lgh000322 <kang000322@gmail.com>
2024-08-20 17:35:17 +0900

    Feat: 로그 추적기 V2 - 동기화 by ThreadLocal (동시성 문제 해결)

---
+++ 스프링 핵심원리 -고급편/advanced/advanced/src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,49 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    public void field() throws Exception{
+        log.info("main start");
+
+        Runnable userA = () -> {
+            service.logic("로직1");
+        };
+
+        Runnable userB = () -> {
+            service.logic("로직2");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setDaemon(true);
+        threadA.setName("thread-1");
+
+        Thread threadB = new Thread(userB);
+        threadA.setDaemon(true);
+        threadA.setName("thread-2");
+
+        threadA.start();
+//        sleep(2000);
+        sleep(100);
+        threadB.start();
+
+        sleep(3000);
+        log.info("main exit");
+    }
+
+    private void sleep(int mills) {
+        try {
+            Thread.sleep(mills);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
