9211d5b9a6c55a45b05dc5f44c49686543f7191f
lgh000322 <kang000322@gmail.com>
2024-08-21 14:09:41 +0900

    Test: 전략패턴

---
+++ 스프링 핵심원리 -고급편/advanced/advanced/src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -0,0 +1,50 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV1Test {
+    @Test
+    public void strategyV0() throws Exception{
+        logic1();
+        logic2();
+    }
+    
+    @Test
+    public void strategyV1() throws Exception{
+        Strategy strategyLogic1 = new StrategyLogic1();
+        ContextV1 contextV1 = new ContextV1(strategyLogic1);
+        contextV1.execute();
+
+        Strategy strategyLogic2 = new StrategyLogic2();
+        ContextV1 contextV2 = new ContextV1(strategyLogic2);
+        contextV2.execute();
+    }
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행(핵심 기능)
+        log.info("비즈니스 로직 1 실행");
+        //비즈니스 로직 종료(핵심 기능 종료)
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행(핵심 기능)
+        log.info("비즈니스 로직 2 실행");
+        //비즈니스 로직 종료(핵심 기능 종료)
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+}
