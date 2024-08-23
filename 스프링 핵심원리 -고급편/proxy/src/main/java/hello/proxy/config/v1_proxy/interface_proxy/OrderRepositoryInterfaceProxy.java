ff67f4135b0d9ae5c9e5b5a889c0930bdfd299b9
lgh000322 <kang000322@gmail.com>
2024-08-23 13:45:10 +0900

    Feat: decorator 패턴 적용

---
+++ 스프링 핵심원리 -고급편/proxy/src/main/java/hello/proxy/config/v1_proxy/interface_proxy/OrderRepositoryInterfaceProxy.java
@@ -0,0 +1,27 @@
+package hello.proxy.config.v1_proxy.interface_proxy;
+
+import hello.proxy.app.v1.OrderRepositoryV1;
+import hello.proxy.app.v1.OrderRepositoryV1Impl;
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class OrderRepositoryInterfaceProxy implements OrderRepositoryV1 {
+
+    private final OrderRepositoryV1Impl target;
+    private final LogTrace trace;
+
+    @Override
+    public void save(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepository.save()");
+            target.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
