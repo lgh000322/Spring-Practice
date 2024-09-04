dbde26373d40a9855a8e53595451a72785ffaa05
lgh000322 <kang000322@gmail.com>
2024-09-04 22:50:24 +0900

    Feat: proxyFactory 적용

---
+++ 스프링 핵심원리 -고급편/proxy/src/main/java/hello/proxy/config/v3_proxyfactory/advice/LogTraceAdvice.java
@@ -0,0 +1,34 @@
+package hello.proxy.config.v3_proxyfactory.advice;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+import java.lang.reflect.Method;
+
+public class LogTraceAdvice implements MethodInterceptor {
+
+    private final LogTrace trace;
+
+    public LogTraceAdvice(LogTrace trace) {
+        this.trace = trace;
+    }
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        TraceStatus status = null;
+        try {
+            Method method = invocation.getMethod();
+            String message = method.getDeclaringClass().getSimpleName() + "." + method.getName() + "()";
+            status = trace.begin(message);
+
+            Object result = invocation.proceed();
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+}
