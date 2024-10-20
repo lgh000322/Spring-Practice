fe06571e360b24ae990f918ff0b6252bda9dc763
이경훈 <100902332+lgh000322@users.noreply.github.com>
2024-10-20 19:44:05 +0900

    jvm 옵션 설정

---
+++ 스프링부트 핵심원리/external/src/test/java/hello/external/JavaSystemProperties.java
@@ -0,0 +1,30 @@
+package hello.external;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Properties;
+
+@Slf4j
+public class JavaSystemProperties {
+
+    public static void main(String[] args) {
+
+        System.setProperty("helloKey", "helloValue");
+
+        Properties properties = System.getProperties();
+        for (Object key : properties.keySet()) {
+            log.info("prop {} = {}", key, System.getProperty(String.valueOf(key)));
+        }
+
+        String url = System.getProperty("url");
+        String username = System.getProperty("username");
+        String password = System.getProperty("password");
+        String helloKey = System.getProperty("helloKey");
+
+        log.info("url = {}", url);
+        log.info("username = {}", username);
+        log.info("password = {}", password);
+        log.info("helloKey = {}", helloKey);
+
+    }
+}
