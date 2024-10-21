6a8703c56fcf44c3b40069837276c55be4682957
이경훈 <100902332+lgh000322@users.noreply.github.com>
2024-10-21 23:37:25 +0900

    ConfigurationProperties V2~V3

--- 스프링부트 핵심원리/external-read/src/main/java/hello/ExternalReadApplication.java
+++ 스프링부트 핵심원리/external-read/src/main/java/hello/ExternalReadApplication.java
@@ -1,8 +1,6 @@
 package hello;
 
-import hello.config.MyDataSourceConfigV1;
-import hello.config.MyDataSourceEnvConfig;
-import hello.config.MyDataSourceValueConfig;
+import hello.config.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -11,8 +9,10 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication(scanBasePackages = "hello.datasource")
 //@Import(MyDataSourceEnvConfig.class)
 //@Import(MyDataSourceValueConfig.class)
-@Import(MyDataSourceConfigV1.class)
-@ConfigurationPropertiesScan
+//@Import(MyDataSourceConfigV1.class)
+//@Import(MyDataSourceConfigV2.class)
+@Import(MyDataSourceConfigV3.class)
+//@ConfigurationPropertiesScan
 public class ExternalReadApplication {
 
 
