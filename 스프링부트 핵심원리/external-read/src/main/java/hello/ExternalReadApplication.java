c87941e1f54556e7a8514e6f0169069de92f99a1
이경훈 <100902332+lgh000322@users.noreply.github.com>
2024-10-21 14:08:28 +0900

    Value 설정 사용

--- 스프링부트 핵심원리/external-read/src/main/java/hello/ExternalReadApplication.java
+++ 스프링부트 핵심원리/external-read/src/main/java/hello/ExternalReadApplication.java
@@ -1,12 +1,14 @@
 package hello;
 
 import hello.config.MyDataSourceEnvConfig;
+import hello.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = "hello.datasource")
-@Import(MyDataSourceEnvConfig.class)
+//@Import(MyDataSourceEnvConfig.class)
+@Import(MyDataSourceValueConfig.class)
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
